bcd40c47f472318d606455375c3867602d3220d9
Anna Ostrowska <anostrowska11@wp.pl>
2026-04-28 00:22:35 +0200

    feat: add data loading function in data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def load_project_data(data_dir="..\\data"):
+   
+    x_train_path = os.path.join(data_dir, "x_train.txt")
+    y_train_path = os.path.join(data_dir, "y_train.txt")
+    x_test_path = os.path.join(data_dir, "x_test.txt")
+    
+    X_train = pd.read_csv(x_train_path, sep='\t')
+    y_train = pd.read_csv(y_train_path, sep='\t')
+    X_test = pd.read_csv(x_test_path, sep='\t')
+    logger.info(f"Data loaded. X_train: {X_train.shape}, y_train: {y_train.shape}, X_test: {X_test.shape}")
+    
+    return X_train, y_train, X_test
